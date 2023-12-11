2bb471f6292c2a5a4f4fb3ca29eff10f3417de11
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2023-12-11 14:39:39 +0100

    Update Page 38.cpp

--- Chapter 2/Page 38.cpp
+++ Chapter 2/Page 38.cpp
@@ -11,8 +11,8 @@ Numbers from 0 to n^2 - 1 are arranged in a square. For example, for n = 5, the 
 Write a program that calculates the sum of numbers in a given row and a given column of the square (and we will count them from zero).
 
 Input: The following integers are read from the standard input (each in a separate line).
-- n - the dimension of the square (1 < n ≤ 10^9)
-- i - the row and column number for which the sum is required (0 ≤ i < n)
+- n - the dimension of the square (1 < n = 10^9)
+- i - the row and column number for which the sum is required (0 = i < n)
 
 Output: Print two integers to the standard output, each on a separate line:
 - the sum of the numbers in the i-th row of the square, and
@@ -26,7 +26,6 @@ Input:
 Output:
 35
 55
-
 */
 
 #include <bits/stdc++.h>
@@ -34,17 +33,30 @@ using namespace std;
 
 typedef long long ll;
 
-ll arithmeticSum(ll row)
+ll ColumnSum(int n, int i)
 {
-    ll a1 = (row - 1) * (row - 1) + 1;
-    ll n = 2 * row - 1;
-    return n * a1 + (n - 1) * n / 2;
+	ll a1 = i;
+	
+	ll sum = n * (2 * a1 + (n - 1) * n) / 2;
+	
+	return sum;
 }
+
+ll RowSum(int n, int i)
+{
+	ll a1 = i * n;
+	
+	ll sum = float(n) * (2 * a1 + (n - 1)) / 2;
+	
+	return sum;
+}
+
 
 int main()
 {
-    ll row;
-    cin >> row;
-
-    cout << arithmeticSum(row);
+	int n, i;
+	cin >> n >> i;
+	
+	cout << RowSum(n, i) << endl;
+	cout << ColumnSum(n, i) << endl;
 }
