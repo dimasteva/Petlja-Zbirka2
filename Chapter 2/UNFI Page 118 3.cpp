ab90044dcdd70ae1515905041280a2ebba381bba
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2023-12-30 19:18:39 +0100

    Create UNFI Page 118 3.cpp

---
+++ Chapter 2/UNFI Page 118 3.cpp
@@ -0,0 +1,30 @@
+/*
+Task: Correct Phones
+A sequence of phone numbers is correct if no number is a prefix of another (meaning there are no two identical numbers in the sequence). Write a program that determines whether the input sequence of numbers is correct.
+
+Input: The standard input provides the number of phone numbers, n (1 ≤ n ≤ 50000), followed by n lines containing n phone numbers. Each number consists of between 3 and 50 digits.
+
+Output: Print "da" if the sequence is correct, i.e., "ne" if it is not.
+
+Example 1
+Input
+4
+192
+194
+199342
+192865
+
+Output
+ne
+
+Example 2
+Input
+4
+199342
+193865
+192
+194
+
+Output
+da
+*/
