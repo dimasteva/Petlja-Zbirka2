37354e6b12634ba1e5ebfddfa095d6b2b8cfd281
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2023-12-31 11:20:15 +0100

    Create Page 125.cpp

---
+++ Chapter 2/Page 125.cpp
@@ -0,0 +1,22 @@
+/*
+Task: Squares
+
+Given a set of points in the plane, write a program that determines how many different squares can be formed with all four vertices belonging to this set of points.
+
+Input: The standard input first reads an integer n (1 ≤ n ≤ 1000), followed by the coordinates of n points in the next n lines (first x, then y, separated by a single space).
+
+Output: Print on the standard output the requested number of squares.
+
+Example:
+Input
+7
+-1 1
+0 1
+1 1
+-1 0
+0 0
+1 0
+0 -1
+Output
+3
+*/
