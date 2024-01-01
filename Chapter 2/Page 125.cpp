2aec21d3404ac9e6ce1d1149a776610cdaa01dc6
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-01-01 16:25:35 +0100

    Update Page 125.cpp

--- Chapter 2/Page 125.cpp
+++ Chapter 2/Page 125.cpp
@@ -20,3 +20,64 @@ Input
 Output
 3
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+typedef pair<int, int> Point;
+
+Point translate(const Point& p, int dx, int dy) {
+    return make_pair(p.first + dx, p.second + dy);
+}
+
+void getOtherTwoVerticesOfSquare(const Point& p1, const Point& p2,
+                                 Point& p3_1, Point& p4_1,
+                                 Point& p3_2, Point& p4_2) {
+    int dx = p2.first - p1.first, dy = p2.second - p1.second;
+    p3_1 = translate(p1, -dy, dx);
+    p4_1 = translate(p2, -dy, dx);
+    p3_2 = translate(p1, dy, -dx);
+    p4_2 = translate(p2, dy, -dx);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<Point> points(n);
+    for (int i = 0; i < n; i++)
+        cin >> points[i].first >> points[i].second;
+    
+    sort(points.begin(), points.end());
+    
+    int squareCount = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            Point p3_1, p4_1, p3_2, p4_2;
+            getOtherTwoVerticesOfSquare(points[i], points[j], p3_1, p4_1, p3_2, p4_2);
+            
+            if (binary_search(points.begin(), points.end(), p3_1) &&
+                binary_search(points.begin(), points.end(), p4_1))
+                squareCount++;
+            
+            if (binary_search(points.begin(), points.end(), p3_2) &&
+                binary_search(points.begin(), points.end(), p4_2))
+                squareCount++;
+        }
+    }
+    
+    cout << squareCount / 4 << endl;
+}
