d4f2d43babc2cc9c8e347b4c23f929086184e084
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-01-14 18:55:27 +0100

    Update Page 35 2.cpp

--- Chapter 2/Page 35 2.cpp
+++ Chapter 2/Page 35 2.cpp
@@ -12,7 +12,7 @@ You are tasked with determining the sum of numbers in a given row of this triang
 
 Input:
 
-Row number k (1 = k = 5 × 10^5), representing the row of the triangle for which the sum should be calculated (counting starts from 1).
+Row number k (1 = k = 5 Ã— 10^5), representing the row of the triangle for which the sum should be calculated (counting starts from 1).
 Output:
 
 The sum of values in the specified row of the triangle.
@@ -27,21 +27,38 @@ Output
 */
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 
 ll arithmeticSum(ll row)
 {
     ll a1 = (row - 1) * (row - 1) + 1;
     ll n = 2 * row - 1;
-    return n * a1 + (n - 1) * n / 2;
+    ll an = a1 + n - 1;
+    return n * (a1 + an) / 2;
 }
 
 int main()
 {
-    ll row;
-    cin >> row;
-
-    cout << arithmeticSum(row);
+	FIO;
+	int n; cin >> n;
+	while(n--)
+	{
+		ll row;
+	    cin >> row;
+		
+	    cout << arithmeticSum(row) << endl;
+	}
 }
