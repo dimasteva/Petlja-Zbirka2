5b5d56ee8be21fbeca11c8bf89341ce15c2acb1c
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-01-14 18:53:25 +0100

    Update Page 33.cpp

--- Chapter 2/Page 33.cpp
+++ Chapter 2/Page 33.cpp
@@ -20,23 +20,50 @@ Output:
 */
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
 typedef long long ll;
 
-float calculate(int n, int a, int d)
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+long long Gauss(long long n)
 {
-    return (float(n) * a + float(n * (n - 1) / 2) * d) / 1000;
+    return n * (n + 1) / 2;
+}
+
+long long ArithmeticSeriesSum(int a, int d, int n)
+{
+    return static_cast<long long>(n) * a + d * Gauss(n - 1);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    int a, d;
-    while (cin >> a >> d)
+    cin.ignore();
+
+    string line;
+    while (getline(cin, line))
     {
-        cout << fixed << setprecision(2) << calculate(n, a, d) << endl;
+        stringstream ss(line);
+        int a, d;
+        ss >> a >> d;
+
+        long long S = ArithmeticSeriesSum(a, d, n);
+        double Skm = static_cast<double>(S) / 1000.0;
+        cout << fixed << setprecision(2) << Skm << endl;
     }
+
     return 0;
 }
