f9614e58bd59f5b0b8dfdb8e0772a012cda036b5
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-01-22 07:21:00 +0100

    Create Page 195 (not solved).cpp

---
+++ Chapter 2/Page 195 (not solved).cpp
@@ -0,0 +1,10 @@
+/*
+Task: Harmonic Pi
+The sum
+
+\[ \sum_{k=0}^{\infty} \frac{(-1)^k}{2k + 1} = 1 - \frac{1}{3} + \frac{1}{5} - \frac{1}{7} + \frac{1}{9} - \frac{1}{11} + \ldots \]
+
+is equal to \( \frac{\pi}{4} \). Write a program that, for the given \( n \), estimates the value of \( \pi \) by calculating the first \( n \) terms of the previous sum. Then, show how the value changes by varying the number of terms, displaying the sum values for each number of terms from 1 to \( n \).
+Note that this is a very poor method for estimating the value of \( \pi \), as you will see, it requires a large number of terms to approach the actual value of \( \pi \).
+
+*/
