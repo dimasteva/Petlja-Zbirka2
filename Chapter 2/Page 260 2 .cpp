25716b986be44193b027599fa7444b79972ebaaf
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-02-01 09:20:22 +0100

    Update and rename Page 260 2 (not solved).cpp to Page 260 2 .cpp

--- Chapter 2/Page 260 2 .cpp
+++ Chapter 2/Page 260 2 .cpp
@@ -28,3 +28,44 @@ Input
 Output
 33
 */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+int main()
+{
+	int n; cin >> n;
+	vector <int> a(n);
+	for (int &x : a)
+		cin >> x;
+	int m; cin >> m;
+	sort(all(a), greater<int>());
+	vector <int> mapa(n + 1, 0);
+	vector <pair<int, int>> ranges(m);
+	for(int i = 0; i < m; i++)
+	{
+		int l, d; cin >> l >> d;
+		ranges[i] = {l-1, d};
+		mapa[l-1]++;
+		mapa[d]--;
+	}
+	for(int i = 1; i < n; i++)
+		mapa[i] += mapa[i-1];
+	sort(all(mapa), greater<int>());
+	ll sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += (ll)mapa[i] * a[i];
+	cout << sum;
+}
