3c2f7969e053d748953c703fe16bbc319cfd1f86
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-02-05 17:38:31 +0100

    Create Page 294.cpp

---
+++ Chapter 2/Page 294.cpp
@@ -0,0 +1,82 @@
+/*
+Task: Conference
+
+During a conference, there are n lectures held in a single day. There are m computer companies represented by numbers from 1 to m, and some companies may hold multiple lectures. A conference attendee wants to attend lectures from all companies without leaving the lecture hall. Write a program that determines the minimum number of lectures the attendee needs to listen to in order to hear at least one lecture from each company.
+
+Input: The standard input contains the number of companies m (1 ≤ m ≤ 50) in the first line, the total number of lectures n (1 ≤ n ≤ 50000) in the second line, and the order of company lectures in the third line, with their ordinal numbers (from 1 to m) separated by spaces.
+
+Output: Print the minimum number of lectures the attendee needs to listen to continuously in order to hear at least one lecture from each company. If it is not possible for the attendee to hear lectures from each company continuously, print "ne moze" (without quotes).
+
+Example 1:
+Input
+4
+20
+4 2 4 3 3 2 2 4 2 2 3 3 1 3 3 1 4 4 1 4
+Output
+6
+Explanation: It is possible to listen to lectures from companies 4, 2, 2, 3, 3, 1.
+
+Example 2:
+Input
+3
+10
+1 2 1 2 1 2 1 2 1 2
+Output
+ne moze
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+int main()
+{
+    FIO;
+
+    int m, n; cin >> m >> n;
+    vector<int> a(n);
+    for (int &x : a)
+        cin >> x;
+
+    vector<int> freq(m+1, 0);
+    int distinct = 0;
+    int start = 0, end = 0;
+    int mn = INT_MAX;
+
+    while (end < n) {
+        if (freq[a[end]] == 0) {
+            distinct++;
+        }
+        freq[a[end]]++;
+        end++;
+
+        while (distinct == m) {
+            mn = min(mn, end - start);
+            freq[a[start]]--;
+            if (freq[a[start]] == 0) {
+                distinct--;
+            }
+            start++;
+        }
+    }
+
+    if (mn == INT_MAX) {
+        cout << "ne moze";
+    } else {
+        cout << mn;
+    }
+
+    return 0;
+}
+
