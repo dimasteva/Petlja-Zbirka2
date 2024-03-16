fa5894f7a9c7f64b7440b82a5d31901e9f66599e
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-03-16 17:00:39 +0100

    Create Page 383 (not solved).cpp

---
+++ Chatpter3/Page 383 (not solved).cpp
@@ -0,0 +1,20 @@
+/*
+Task: Numbers without Consecutive Zeros in Binary Representation
+
+Write a program to display the decimal representations of all natural numbers that have at most n binary digits and do not have two consecutive zeros in their binary representation.
+
+Input: The first line of the standard input contains a natural number n (1 ≤ n ≤ 20).
+
+Output: Display the requested numbers in ascending order on the standard output, with each number on a separate line.
+
+Example
+Input
+3
+Output
+1
+2
+3
+5
+6
+7
+*/
