15a93cfd3749bb1557fcf89868bb186b83dd211f
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-04-15 15:31:39 +0200

    Create Page 433.cpp

---
+++ Chatpter3/Page 433.cpp
@@ -0,0 +1,68 @@
+/*
+Task: Arranging n Queens on a Chessboard
+Write a program that determines all positions of n queens on an n × n chessboard such that no two queens attack each other. To prevent queens from attacking each other, there must be exactly one queen in each row, and no two queens are in the same column. Therefore, the arrangement is determined by an array of n distinct numbers from 1 to n representing the column numbers in which the queens are located in rows from 1 to n.
+
+Input: Enter the number n from standard input (4 ≤ n ≤ 11).
+
+Output: Print all possible arrangements of queens to standard output (in any order).
+
+Example:
+Input:
+4
+
+Output:
+3 1 4 2
+2 4 1 3
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define MOD 1000000007
+#define all(c) (c).begin(), (c).end()
+
+ll sum = 0;
+bool QueensAttacking(vector<int>& p, int k)
+{
+    forn(i, k)
+    {
+        if (p[i] == p[k])
+            return true;
+        if (abs(k - i) == abs(p[k] - p[i]))
+            return true;
+    }
+    return false;
+}
+
+void nQueen(vector<int>& p, int k)
+{
+    sum++;
+    if (k == p.size())
+    {
+        for (int x : p)
+            cout << x + 1 << " ";
+        cout << endl;
+    } else
+    {
+        forn(i, p.size())
+        {
+            p[k] = i;
+            if (!QueensAttacking(p, k))
+                nQueen(p, k + 1);
+        }
+    }
+}
+
+void nQueen(int n)
+{
+    vector<int> p(n);
+    nQueen(p, 0);
+}
+
+int main()
+{
+    int n; cin >> n;
+    nQueen(n);
+    cout << sum << endl;
+}
