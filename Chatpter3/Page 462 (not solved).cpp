7751f8d7705c94333fad378b2443e1c9fb907921
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-04-20 13:23:47 +0200

    Create Page 462 (not solved).cpp

---
+++ Chatpter3/Page 462 (not solved).cpp
@@ -0,0 +1,22 @@
+/*
+Task: Removing incorrect brackets
+A string contains, among other characters, small brackets (( and )), but it's possible that they are not correctly paired. The goal is to delete as few characters as possible from the string to obtain a string with correctly paired brackets. Write a program that prints all possible strings with correctly paired brackets obtained by deleting as few characters as possible.
+
+Input: The input is a string of maximum length 50 characters.
+
+Output: Print all the requested strings in lexicographical order.
+
+Example 1
+Input
+()())()
+Output
+(())()
+()()()
+
+Example 2
+Input
+(abc()+def)))(
+Output
+(abc()+def)
+(abc(+def))
+*/
