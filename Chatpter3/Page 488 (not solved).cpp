5b61adb10f0372044c5c03fd7b8de5cd3a0f41d2
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-05-20 13:48:36 +0200

    Update Page 488 (not solved).cpp

--- Chatpter3/Page 488 (not solved).cpp
+++ Chatpter3/Page 488 (not solved).cpp
@@ -15,3 +15,66 @@ Input
 Output
 ne
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n)          for (int i = 0; i < int(n); i++)
+#define forn1(i, n)          for (int i = 1; i < int(n); i++)
+#define pb                  push_back
+#define MOD                 1000000007 // 998244353
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(c) (c).begin(), (c).end()
+
+void msort(vector<pair<int, int>>& a, int l, int r, vector<pair<int, int>>& b, map<int, int>& m)
+{
+    if (l >= r)
+        return;
+    int mid = l + (r - l) / 2;
+    msort(a, l, mid, b, m);
+    msort(a, mid + 1, r, b, m);
+    int pl = l, pr = mid + 1, inc = 0;
+    while(pl <= mid && pr <= r)
+    {
+        if (a[pl].first <= a[pr].first)
+        {
+            m[a[pl].second] += r - pr + 1;
+            b[inc++] = a[pl++];
+        } else
+        {
+            b[inc++] = a[pr++];
+        }
+    }
+    while(pl <= mid)
+        b[inc++] = a[pl++];
+    while(pr <= r)
+        b[inc++] = a[pr++];
+    
+    copy(begin(b), next(begin(b), r - l + 1), next(begin(a), l));
+}
+
+int main()
+{
+    int n; cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].first;
+        a[i].second = i;
+    }
+    vector<pair<int, int>> b = a;
+    map<int, int> m;
+    msort(a, 0, n - 1, b, m);
+    int q; cin >> q;
+    while(q--)
+    {
+        int d, k; cin >> d >> k;
+        if (m[d] > k)
+            cout << "ne" << endl;
+        else
+            cout << "da" << endl;
+    }
+}
