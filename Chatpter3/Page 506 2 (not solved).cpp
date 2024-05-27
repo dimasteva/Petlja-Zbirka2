5bff2d781502af01952b3ddd7374ca9ee8dbe9ce
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-05-27 16:44:51 +0200

    Create Page 506 2 (not solved).cpp

---
+++ Chatpter3/Page 506 2 (not solved).cpp
@@ -0,0 +1,16 @@
+/*
+Task: Nearest Pair of Points
+In the given set of points in the plane, determine the distance between the two points that are closest to each other.
+Input: The standard input consists of the number of points n (1 ≤ n ≤ 50000), followed by the coordinates of the points in the next n lines (two integers between −109 and 109, separated by a space).
+Output: Print the requested distance to the standard output, rounded to five decimal places.
+Example
+Input
+5
+0 0
+0 2
+2 0
+2 2
+1 1
+Output
+1.41421
+*/
