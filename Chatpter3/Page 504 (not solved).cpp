a76410cd6b9e942559d27fee24760591b83d5612
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-05-27 13:12:02 +0200

    Create Page 504 (not solved).cpp

---
+++ Chatpter3/Page 504 (not solved).cpp
@@ -0,0 +1,17 @@
+/*
+Task: Maximum Subarray Sum
+Write a program that determines the maximum sum of any segment (subarray of consecutive elements) of a given array.
+Input: The standard input consists of an integer n (1 ≤ n ≤ 50,000), followed by n integers between -10 and 10, each number on a separate line.
+Output: Print the required maximum sum on the standard output.
+Example
+Input
+6
+2
+-3
+4
+-1
+3
+-2
+Output
+6
+*/
