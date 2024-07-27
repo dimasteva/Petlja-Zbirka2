10a532f7c18a844dcf0fd9f66fd6bf89f7d549e4
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-07-27 15:10:06 +0200

    Create Page 512 (not solved).cpp

---
+++ Chatpter3/Page 512 (not solved).cpp
@@ -0,0 +1,18 @@
+/*
+Task: Triminos
+Given an 8 × 8 board with one missing square, the task is to fill the remaining 63 squares with triminos (shapes formed by removing one square from a 2 × 2 square).
+Input: Two integers between 0 and 7, separated by a space, representing the coordinates (row and column) of the missing square on the board are read from standard input.
+Output: Print a character matrix representing the board covered with triminos. A space should be printed in the position of the missing square, and different characters (for example, lowercase English letters) should be used to represent the triminos. The solution is not unique.
+Example
+Input
+3 5
+Output
+cceemmoo
+cbbemllo
+dbffnnlp
+ddfan pp
+hhjaartt
+hgjjrrqt
+iggksqqu
+iikkssuu
+*/
