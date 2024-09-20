4a2ce3f2d1a5d5a69d5cf1e0c16b7a297c1b8af8
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-09-20 15:27:56 +0200

    Create Page 580 (not solved).cpp

---
+++ Chatpter3/Page 580 (not solved).cpp
@@ -0,0 +1,17 @@
+/*
+Task: Longest Palindromic Subsequence
+Write a program to determine the length of the longest subsequence of a given string s that is a palindrome.
+The subsequence does not need to contain consecutive characters of the string, but they must appear in the same order
+(a subsequence is obtained by deleting an arbitrary number of characters).
+
+Input: A string s composed only of lowercase English letters is read from standard input, with a maximum length of 5000 characters.
+
+Output: Print only the required length of the longest palindrome subsequence.
+
+Example
+Input
+najduzipalindrom
+
+Output
+5
+*/
