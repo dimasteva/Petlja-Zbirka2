093f3ca3645b606f37ab10cdf7f394ac049d05d9
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-02 11:43:20 +0200

    Create Page 598 (not solved).cpp

---
+++ Chatpter3/Page 598 (not solved).cpp
@@ -0,0 +1,25 @@
+/*
+Task: Transporting items by elevator
+
+In front of an elevator with a known carrying capacity, there are items with known masses. Write a program that determines the minimum number of trips required to transport all the items.
+
+Input: From the standard input, the number of items n (1 ≤ n ≤ 20) is read, followed by the masses of the items (integers between 1 and 50), and finally, the elevator’s carrying capacity (an integer between 50 and 120).
+
+Output: On the standard output, print the required number of trips.
+
+Example
+
+Input
+7
+14
+18
+27
+47
+37
+11
+22
+70
+
+Output
+3
+*/
