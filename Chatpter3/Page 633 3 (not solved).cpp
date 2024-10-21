da8bfbc67d35041829aab8836e322a3905d8ac20
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-21 15:13:36 +0200

    Create Page 633 3 (not solved).cpp

---
+++ Chatpter3/Page 633 3 (not solved).cpp
@@ -0,0 +1,20 @@
+/*
+Task: Sorting numbers
+Write a program that arranges (sorts) an array of numbers in non-decreasing order (each subsequent number must be greater than or equal to the previous one).
+Input: Read the number n (1 ≤ n ≤ 50,000) followed by n natural numbers less than 2n, each on a separate line.
+Output: Print the read numbers in sorted order to standard output.
+Example
+Input
+5
+3
+1
+6
+8
+1
+Output
+1
+1
+3
+6
+8
+*/
