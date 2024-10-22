47830e64dc37204f9f41c2374dff64f9a1ad5a02
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-22 19:47:01 +0200

    Create Page 636 3 (not solved).cpp

---
+++ Chatpter3/Page 636 3 (not solved).cpp
@@ -0,0 +1,11 @@
+/*
+Task: Converting a fully parenthesized expression to postfix form  
+Write a program that converts a valid infix arithmetic expression with parentheses around each application of a binary operator into postfix form. For simplicity, assume that all operands are single-digit numbers and that only addition and multiplication operations are used.  
+Input: A single line of standard input contains a valid, fully parenthesized expression.  
+Output: Print the desired postfix form to standard output.  
+Example  
+Input  
+(((3*5)+(7+(2*1)))*4)  
+Output  
+35*721*++4*  
+*/
