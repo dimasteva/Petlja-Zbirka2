63c2f18685c9f622036526e8216f2a659f8bdf01
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-23 17:08:33 +0200

    Update Page 640 (not solved).cpp

--- Chatpter3/Page 640 (not solved).cpp
+++ Chatpter3/Page 640 (not solved).cpp
@@ -1,1 +1,21 @@
-
+/*
+Task: Value of the min-max expression  
+Expressions are built from digits, parentheses, and two infix operators. The operator `m` denotes finding the minimum of two numbers, and `M` denotes the maximum of two numbers. The operator `M` has a higher precedence. Write a program that calculates the value of the given expression.  
+Input: The input consists of a single expression of the described form (in one line, without spaces).  
+Output: Output the value of the given expression.  
+Example 1  
+Input  
+3m4M5  
+Output  
+3  
+Example 2  
+Input  
+((((9M1)M(0m6)m0M0)m0M6m(9m1M3m(4m(6m8))))M8M5)m(1M2M(0m6))  
+Output  
+2  
+Example 3  
+Input  
+7  
+Output  
+7
+*/
