81c52b4bb7697a9ad3e8447a03a1768610efd4a4
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-24 14:48:46 +0200

    Create Page 648 (not solved).cpp

---
+++ Chatpter3/Page 648 (not solved).cpp
@@ -0,0 +1,22 @@
+/*
+Task: Longest Segment Containing Consecutive Numbers  
+In an array of distinct integers, find the length of the longest segment (consisting of consecutive elements of the array) that contains numbers that can be arranged into a sequence of consecutive integers.  
+Input: The first line of standard input contains the number of elements in the array n (1 ≤ n ≤ 10000), followed by the next n lines containing the elements of the array in order.  
+Output: On standard output, print the length of the longest segment whose elements can be arranged into a sequence of consecutive integers.  
+Example  
+Input  
+11  
+55  
+56  
+58  
+57  
+90  
+92  
+94  
+93  
+91  
+59  
+60  
+Output  
+5
+*/
