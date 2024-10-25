ed1516bf4daed4036e06e9fe8bd421edd5b0e669
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-25 21:16:40 +0200

    Create Page 656 (not solved).cpp

---
+++ Chatpter3/Page 656 (not solved).cpp
@@ -0,0 +1,17 @@
+/*
+Task: Powers of Natural Numbers  
+Consider a series of numbers that are powers of natural numbers (with the base and exponent strictly greater than 1), sorted in ascending order. Its initial part is as follows: a₀ = 4 = 2², a₁ = 8 = 2³, a₂ = 9 = 3², a₃ = 16 = 4² = 2⁴, a₄ = 25 = 5², a₅ = 27 = 3³, a₆ = 32 = 2⁵, and so on. Write a program that determines the element aₖ.  
+Input: The number k (1 ≤ k ≤ 10⁶) is read from standard input.  
+Output: Print the element aₖ on standard output.  
+Example 1  
+Input  
+6  
+Output  
+32  
+Example 2  
+Input  
+1000000  
+Output  
+979850535876
+*/
+
