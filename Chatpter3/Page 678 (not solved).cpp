140392f209dfda009fb2bc800dab7f37c74fb9f3
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-29 23:31:34 +0100

    Create Page 678 (not solved).cpp

---
+++ Chatpter3/Page 678 (not solved).cpp
@@ -0,0 +1,12 @@
+/*
+Task: Nearest Greater Predecessor  
+A sequence of building heights along a street is known. Spider-Man wants to set up a horizontal rope on each building that will lead him to some previous building. The rope can be attached anywhere on the building (except on the roof). For each building, the height of the previous building that the horizontal rope will lead to must be printed (the first building to the left of the current one that is strictly taller than the current one).  
+Input: The number of buildings \( n \) (1 ≤ \( n \) ≤ 50000) is entered from standard input, followed by \( n \) heights of buildings separated by spaces.  
+Output: Print the required sequence of building heights separated by spaces to standard output. If there is no strictly taller building in front of a building, print - .  
+Example  
+Input  
+7  
+2 7 2 4 1 3 6  
+Output  
+- - 7 7 4 4 7  
+*/
