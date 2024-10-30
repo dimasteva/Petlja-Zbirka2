7c01838767c2d0114bf7fe953d7c5e1f85172432
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-30 23:25:04 +0100

    Create Page 682 4 (not solved).cpp

---
+++ Chatpter3/Page 682 4 (not solved).cpp
@@ -0,0 +1,30 @@
+/*
+Task: Sum of Minimums  
+Write a program that determines the sum of the minimums of all non-empty subarrays of a given array of integers.  
+Input: The standard input provides the number `n` (1 ≤ n ≤ 10^5), followed by `n` elements of the array (numbers between 0 and 10^5, each separated by a space).  
+Output: Since the sum can be large, print its remainder when divided by one billion and seven (10^9 + 7) to the standard output.
+
+Example 1:  
+Input  
+3  
+1 3 2  
+Output  
+10  
+Explanation  
+The non-empty subarrays are [1], [1, 3], [1, 3, 2], [3], [3, 2], and [2]. Their minimums are 1, 1, 1, 3, 2, and 2, respectively, and the sum of these minimums is 10.
+
+Example 2:  
+Input  
+7  
+4 3 2 1 2 3 4  
+Output  
+48  
+
+Example 3:  
+Input  
+7  
+1 2 3 3 3 2 1  
+Output  
+49
+*/
+
