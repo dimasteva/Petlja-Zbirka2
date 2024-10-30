04abadab6e26cc4bdf2c2c3b4d945dc2a8149833
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-30 23:10:08 +0100

    Update Page 682 2 (not solved).cpp

--- Chatpter3/Page 682 2 (not solved).cpp
+++ Chatpter3/Page 682 2 (not solved).cpp
@@ -10,3 +10,51 @@ Input
 Output  
 20
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+
+int main()
+{
+    int n; cin >> n;
+    vector<int> a(n);
+    forn(i, n)
+    	cin >> a[i];
+    
+    stack<int> indices;
+    ll maxArea = 0;
+    
+    forn(i, n)
+    {
+    	while(!indices.empty() && a[indices.top()] > a[i])
+    	{
+    		int height = a[indices.top()];
+    		indices.pop();
+    		int width = indices.empty() ? i : i - indices.top() - 1;
+    		maxArea = max(maxArea, (ll)height * width);
+		}
+		indices.push(i);
+	}
+	while(!indices.empty())
+	{
+		int height = a[indices.top()];
+		indices.pop();
+		int width = indices.empty() ? n : n - indices.top() - 1;
+		maxArea = max(maxArea, (ll)height * width);
+	}
+	cout << maxArea << endl;
+}
