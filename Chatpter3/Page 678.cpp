86b0770e2135b6251caddc7588628a278993e104
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-30 08:55:58 +0100

    Update and rename Page 678 (not solved).cpp to Page 678.cpp

--- Chatpter3/Page 678.cpp
+++ Chatpter3/Page 678.cpp
@@ -10,3 +10,36 @@ Input
 Output  
 - - 7 7 4 4 7  
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+#define MOD 1000000007 // 998244353
+#define FIO                \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+int main()
+{
+    int n; cin >> n;
+    stack<int> s;
+    forn(i, n)
+    {
+    	int x; cin >> x;
+    	while(!s.empty() && s.top() <= x)
+    		s.pop();
+    	if (s.empty())
+    		cout << "- ";
+    	else
+    		cout << s.top() << " ";
+    	s.push(x);
+	}
+}
