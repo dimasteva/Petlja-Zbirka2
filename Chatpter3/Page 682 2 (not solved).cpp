30cd1435902603f66994501b9c98cb06396d6860
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-10-30 10:12:10 +0100

    Create Page 682 2 (not solved).cpp

---
+++ Chatpter3/Page 682 2 (not solved).cpp
@@ -0,0 +1,12 @@
+/*
+Task: Largest Rectangle in Histogram  
+Write a program that determines the area of the largest rectangle that can be inscribed in a given histogram (the histogram consists of bars of width 1).  
+Input: The number of bars, `n` (1 ≤ n ≤ 50,000), is provided on standard input, followed by the heights of the bars (positive integers separated by spaces).  
+Output: Print the required area on the standard output.  
+Example  
+Input  
+6  
+3 7 4 6 5 8  
+Output  
+20
+*/
