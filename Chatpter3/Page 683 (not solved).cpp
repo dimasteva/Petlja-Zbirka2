9ea4efe2e8188d2329f649dcb0a3ca09a71d22fb
Dimitrije Stevanovic <76264723+dimasteva@users.noreply.github.com>
2024-11-02 00:04:13 +0100

    Create Page 683 (not solved).cpp

---
+++ Chatpter3/Page 683 (not solved).cpp
@@ -0,0 +1,19 @@
+/*
+Task: Segments Bounded by Maxima  
+Determine how many segments (at least two-element contiguous subarrays of the array) exist in an array of distinct numbers where all elements within the segment are strictly less than the elements at their ends.  
+Input: The first input line contains the number n, followed by n distinct natural numbers (each on a separate line).  
+Output: Print the requested number of segments to standard output.  
+Example  
+Input  
+8  
+5  
+3  
+8  
+4  
+2  
+7  
+10  
+6  
+Output  
+11  
+*/
